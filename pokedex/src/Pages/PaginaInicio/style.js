020da31fecb0a72ef84cf4914c842046e1425a00
thayna-saad <thaynasaad.ts@gmail.com>
2022-01-04 19:21:00 -0300

    Inicio Pagina Inici

--- pokedex/src/Pages/PaginaInicio/style.js
+++ pokedex/src/Pages/PaginaInicio/style.js
@@ -1,2 +1,37 @@
-import styledComponents from "styled-components";
+import styled from "styled-components";
 
+export const CardTelaInicial = styled.div `
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+    border: solid 1px black;
+    width: 250px;
+    height:350px;
+    margin: 40px;
+
+    img {
+       
+        width: 100%;
+        height: auto;
+    }
+
+    .CardButton{
+        display: flex;
+        justify-content:space-between;
+        /* background-color: red; */
+        align-items: center;
+        flex-direction: row;
+        
+
+
+        button {
+            width:100%;
+            height:80px;
+
+            :hover {
+                background-color: #2e618a;
+            }
+        }
+    }
+
+`
