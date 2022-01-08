a572c93ad92e28a322c7b321dc4eea7c2918e7e6
Ruanar <ruana.piber@gmail.com>
2022-01-07 21:03:57 -0300

    pokedex começando a funcionar

--- pokedex/src/Pages/PaginaPokedex/style.js
+++ pokedex/src/Pages/PaginaPokedex/style.js
@@ -1,2 +1,77 @@
-import styledComponents from "styled-components";
+import styled from "styled-components";
+
+export const StyledContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    p{
+            text-align: center;
+            font-weight: bold;
+            color: var(--detalhes);
+            transition: 0.9s;
+
+            :hover{
+                color: var(--segundaria);
+            }
+        }
+        
+    .area-card{
+        display: flex;
+        flex-direction: column;
+        place-items: center;
+        justify-content: center;
+        background: var(--branco);
+        margin: 5px;
+        margin-top: 10px;
+        width: 170px;
+        height: 170px;
+        border-radius: 5px;
+        border: 1px solid var(--primaria);
+
+
+
+        .area-card-button{
+            display: flex;
+            button{ 
+                    margin: 2px;
+                    width: 75px;
+                    height: 30px;
+                    border-radius: 5px 5px 5px 5px;
+                    color: var(--branco);
+                    font-weight: bold;
+                    background-color: var(--primaria);
+                    border: 2px solid var(--primaria);
+                    transition: 0.3s;
+                    cursor: pointer;
+
+                    :hover{
+                        background-color: var(--segundaria);
+                        border: 2px solid var(--detalhes);
+                        color: var(--detalhes);
+                    }
+
+                    :active{
+                        background-color: var(--segundaria);
+                        border: 2px solid var(--detalhes);
+                        color: var(--branco);
+                        box-shadow: 0 0 15px var(--segundaria);
+                    }
+                }
+        }
+
+    }
+
+    .area-pokes{
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+
+    .area-button{
+        display: flex;
+        justify-content: center;
+    }
+`
+
 
